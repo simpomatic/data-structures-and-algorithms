1eb6d2639c6b62a18e17727c1ce4ba4c0f39a007
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2020-08-06 21:24:51 -0700

    Change project structure
    Remove individual console projects for sorting algorithms, and unify them under one class library.

---
+++ SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    class InsertionSort
+    {
+        /// <summary>
+        /// Implementation of the Insertion sort algorithm.
+        /// </summary>
+        /// <param name="arr">Unsorted array</param>
+        static void insertionSort(ref int[] arr)
+        {
+            // Iterate through the entire array
+            for (int index = 0; index < arr.Length; index++)
+            {
+                if (index == 0)
+                {
+                    // Do nothing, first element is already sorted
+                }
+                else
+                {
+                    int reverseCounter = index - 1;
+                    int currentIndex = index;
+                    while (reverseCounter >= 0)
+                    {
+                        // Returns the index of the lowest value of the two
+                        int minimumValueIndex = (arr[currentIndex] > arr[reverseCounter]) 
+                            ? reverseCounter : currentIndex;
+                        if (minimumValueIndex == currentIndex)
+                        {
+                            // Swap values
+                            int temp = arr[currentIndex];
+                            arr[currentIndex] = arr[reverseCounter];
+                            arr[reverseCounter] = temp;
+
+                            // Set new current index and reduce reverse iterator counter
+                            currentIndex = reverseCounter--;
+                        }
+                        // No swap needed, avoid checking all previous entries as well
+                        else
+                        {
+                            reverseCounter = -1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SortingAlgorithms/SharedFunctionality.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    public class SharedFunctionality
+    {
+        /// <summary>
+        /// Method to display the array to the console.
+        /// </summary>
+        /// <param name="arr">The given array</param>
+        /// <param name="n">The length of the array</param>
+        public static void printArray(int[] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write(arr[i] + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
