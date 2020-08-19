b84e9e98c03d9a94991b406e6f1085f93f38f30a
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2020-08-18 23:42:33 -0700

    Add `TreeNode` class in the `Trees.Shared` namespace
    All our trees will utilize this `TreeNode` class. Useful utility functions have been added as well as getters/setters for all of TreeNode's properties.

---
+++ BinarySearchTree/Shared/TreeNode.cs
@@ -0,0 +1,57 @@
+﻿namespace Trees.Shared
+{
+    public class TreeNode
+    {
+        #region Fields and constants
+        public int key { get; set; }
+        public string value { get; set; }
+        public TreeNode leftChild { get; set; }
+        public TreeNode rightChild { get; set; }
+        public TreeNode parent { get; set; }
+        #endregion
+
+        public TreeNode(
+            int key,
+            string value,
+            TreeNode leftChild = null,
+            TreeNode rightChild = null,
+            TreeNode parent = null
+        ) {
+            this.key = key;
+            this.value = value;
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+            this.parent = parent;
+        }
+
+        public bool hasLeftChild()
+        {
+            return leftChild != null;
+        }
+
+        public bool hasRightChild()
+        {
+            return rightChild != null;
+        }
+
+        public bool isLeftChild()
+        {
+            return parent != null && parent.leftChild == this;
+        }
+
+        public bool isRightChild()
+        {
+            return parent != null && parent.rightChild == this;
+        }
+
+        public bool isRoot()
+        {
+            return parent == null;
+        }
+
+        public bool isLeaf()
+        {
+            return rightChild == null && leftChild == null;
+        }
+    }
+}
